c22c65d8c7ef91373361fc06b6b90d7814332bec
xx18 <xx18@global.tesco.org>
2017-12-26 17:18:47 +0530

    combine Reducer example

--- src/index.js
+++ src/index.js
@@ -1,35 +1,52 @@
-import { createStore } from "redux";
+import { createStore , combineReducers} from "redux";
 
-const initialState = {
+const mathReducer = (state = {
     result: 1,
     data: []
-}
-
-const reducer = (state = initialState, action) => {
+}, action) => {
     switch (action.type) {
         case "ADD":
             state = {
                 ...state,
                 result: state.result + action.payload,
-                data:[...state.data, action.payload]
+                data: [...state.data, action.payload]
             };
             break;
         case "SUBTRACT":
             state = {
                 ...state,
                 result: state.result - action.payload,
-                data:[...state.data, action.payload]
+                data: [...state.data, action.payload]
             };
             break;
     }
     return state;
 }
 
+const userReducer = (state=
+    {
+        age:27,
+        name:"Max"
+}, action) => {
+    switch (action.type) {
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+        case "SET_NAME":
+            state = {
+                ...state,
+                name:action.payload
+            }
+            break;
+    }    
+    return state;
+}
 
-const store = createStore(reducer, initialState
 
-
-);
+const store = createStore(combineReducers({mathReducer,userReducer}));
 
 store.subscribe(() => {
     console.log("Updated Store", store.getState());
@@ -50,4 +67,14 @@ store.dispatch({
 store.dispatch({
     type: "SUBTRACT",
     payload: 20
+})
+
+store.dispatch({
+    type:"SET_AGE",
+    payload:25
+})
+
+store.dispatch({
+    type:"SET_NAME",
+    payload:"Kavitha"
 })
