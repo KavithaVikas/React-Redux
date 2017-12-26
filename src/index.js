695e88cad3a51de6dcca981ac021aa03b8d232d4
xx18 <xx18@global.tesco.org>
2017-12-26 16:52:13 +0530

    using spread operator

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import { createStore } from "redux";
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    data: []
+}
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                data:[...state.data, action.payload]
+            };
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                data:[...state.data, action.payload]
+            };
+            break;
     }
     return state;
 }
 
 
-const store = createStore(reducer, 1);
+const store = createStore(reducer, initialState
 
-store.subscribe( () => {
+
+);
+
+store.subscribe(() => {
     console.log("Updated Store", store.getState());
 }
 
@@ -22,5 +39,15 @@ store.subscribe( () => {
 
 store.dispatch({
     type: "ADD",
+    payload: 20
+})
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+})
+
+store.dispatch({
+    type: "SUBTRACT",
     payload: 20
 })
